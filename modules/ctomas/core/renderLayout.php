ccde847bdeda60136f2760cda2ddc09d981f16d4
Carlos <ctmdesarrollo@gmail.com>
2015-05-19 22:42:36 +0200

    eliminadas las clases que ya han sido integradas en el FrontController

--- modules/ctomas/core/renderLayout.php
+++
@@ -1,9 +0,0 @@
-<?php
-function renderLayout($layout, $content)
-{
-    ob_start();
-	require_once("../modules/ctomas/views/layouts/".$layout);
-	$response = ob_get_contents();
-	ob_end_clean();
-	return $response;
-}
