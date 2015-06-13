c7ce989c69d91ee3d662a3abfe247490f6aa41fa
Carlos <ctmdesarrollo@gmail.com>
2015-06-13 22:05:04 +0200

    modificandoc contenido ayuda

--- modules/ctomas/views/estaticas/ayuda.html.php
+++ modules/ctomas/views/estaticas/ayuda.html.php
@@ -11,16 +11,20 @@
         <section class="module content">
             <div class="container">
                 <h2 class="title">El panel de control</h2>
-
-                <p>Hola.</p>
+                <p>Tienes que ser un usuario registrado y haber iniciado sesión para poder acceder al panel de control. Desde allí veras listados todos los currículums que has creado.</p>
+				<p>Haciendo click sobre el nombre del CV podrás visualizarlo, aparecerá un menú para elegir con que estilo quieres que se muestre.</p>
+				<p>Desde los botones puedes crear una copia del CV, eliminarlo, descargarlo en formato PDF o acceder a la edición. 
             </div>
         </section>
 
         <section class="module content">
             <div class="container">
                 <h2 class="title">Creando un currículum</h2>
-
-                <p>Hola.</p>
+                <p>En primer lugar hay que elegir sobre nuevo CV e introducir el nombre deseado.Cuando lo envíes aparecerá en tu listado y podrás editar los datos. </p>
+				<p>La edición se divide en áreas: Datos personales, experiencia, formación, idiomas y capacidades. Puedes desplazarte más rápidamente entre ellas 
+				utilizando la barra de navegación superior. Puedes realizar una vista previa del C.V. para ver más claramente los datos introducidos.<p> 
+				<p>La experiencia, la formación, los idiomas y las capacidades se introducen de la misma manera que se crea un CV, primero envías el nombre y a 
+				continuación te aparecerán los datos para editar.</p>
             </div>
         </section>
     </div>
